7c7cf5b0ea3c714eac4b80cfd17cb7d9b7b38b97
Aaronsabu <70811901+Aaronsabu@users.noreply.github.com>
2022-10-29 00:31:40 +0530

    Update MonitorScreen.js

--- src/MonitorScreen.js
+++ src/MonitorScreen.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
-import {Text, View, StyleSheet, Image} from "react-native";
+import {Text, View, StyleSheet, Image, Animated, TouchableOpacity} from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import  LiquidProgress from "react-native-liquid-progress";
+import { StatusBar } from "expo-status-bar";
 import Speedometer from "react-native-speedometer-chart";
 
 const MonitorScreen = () => {
-    //const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
    // const onChange = () => setValue({ value: parseInt(value) });
     return (
-     <View style={styles.container}>
+     <SafeAreaView style={styles.container}>
         <Image style={styles.img} source={require('../src/img/AQUA.png')} />
         <View style = {styles.heading}>
           <Image style={styles.img2} source={require('../src/img/AquaSymbol.png')} />
@@ -15,31 +18,40 @@ const MonitorScreen = () => {
 
      <View style={styles.innerView}>
      <Text style={styles.txt}>Flow Rate</Text>
+
           <Speedometer
-            value={0}
+            value={100}
             totalValue={1000}
-            outerColor="#d3d3d3"
+            outerColor="grey"
             outerStroke="#da2128"
             showText
-            text="0ml/sec"
-            textStyle={{ color: "blue", fontWeight: "bold"}}
-          />  
+            text="100ml/sec"
+            textStyle={{ color: "#00FFFF", fontWeight: "bold"}}
+          /> 
+     
+
           </View>
           <View style={styles.innerView1}>  
           <Text style={styles.txt}>Total Volume</Text>
-          <Speedometer
-            value={0}
-            totalValue={1000}
-            outerColor="#d3d3d3"
-            outerStroke="#da2128"
-            showText
-            text="0m3"
-            textStyle={{ color: "blue", fontWeight: "bold"}}
-          /> 
+          <View style={styles.progress}>
+          <LiquidProgress
+            backgroundColor={"white"}
+            frontWaveColor={"blue"}
+            backWaveColor={"skyblue"}
+            fill={.8}
+            size={500}
+            customMask={<View style={{ backgroundColor: "red", width: 200, height: 200, borderRadius: 360}}></View>}
+          >
+          <Animated.View style={styles.row}>
+            <Text style={styles.text}>{(.8 * 100).toFixed(2)}%</Text>
+          </Animated.View>
+         </LiquidProgress>
+         </View>
+ 
           </View> 
 
-
-     </View>
+          <StatusBar style="auto" />  
+     </SafeAreaView>
     );
 };
 
@@ -52,7 +64,6 @@ MonitorScreen.navigationOptions = () => {
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop:30,
         flex: 1,
         alignItems:"center",
         backgroundColor: "black"
@@ -76,8 +87,17 @@ const styles = StyleSheet.create({
       txt: {
         color:"#00FFFF",
         textAlign:"center",
-        paddingBottom:20,
         paddingRight: 250
+      },
+      row: {
+        alignSelf: "center",
+        flexDirection: "row",
+        height: 70,
+        paddingRight: 50
+      },
+      text: {
+        color: "white",
+        fontSize: 30,
       },
       txt2: {
         fontWeight:"bold",
@@ -90,6 +110,11 @@ const styles = StyleSheet.create({
       },
       innerView1: {
         marginTop: 50
+      },
+      progress: {
+        borderColor: "red",
+        borderWidth:1,
+        paddingLeft: 200
       }
 
 });
